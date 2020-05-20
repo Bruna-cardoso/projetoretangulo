4e33fa8eee114fee589374775e0ca2acfe032ba5
Bruna Cardoso <bruna.carolinepc@gmail.com>
2020-05-20 19:47:21 -0300

    Leitura da altura com validacao

--- src/application/projetoretangulo.java
+++ src/application/projetoretangulo.java
@@ -11,7 +11,7 @@ public class projetoretangulo {
 		Scanner sc = new Scanner(System.in);
 
 		
-		double largura;
+		double largura, altura;
 		
 		// Leitura da largura com validacao
 		
@@ -21,6 +21,17 @@ public class projetoretangulo {
 			System.out.print("O valor deve ser positivo. Tente novamente: ");
 			largura = sc.nextDouble();
 			}
+			
+		// Leitura da altura com validacao
+		
+		System.out.print("Digite a altura do retangulo: ");
+		altura = sc.nextDouble();
+			while (altura <= 0.0) {
+			System.out.print("O valor deve ser positivo. Tente novamente: ");
+			altura = sc.nextDouble();
+			}
+		
+		System.out.println();
 		
 		
 		sc.close();
